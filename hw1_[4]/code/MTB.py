e5f3d2f8e205ed0942ec0e2c0e3f3a9084148f73
Austin <austin030606@gmail.com>
2025-03-21 17:40:02 +0800

    init MTB code

---
+++ hw1_[4]/code/MTB.py
@@ -0,0 +1,12 @@
+import cv2
+
+# input: N 8-bit grayscale images
+# One of the N images is arbitrarily selected as the reference image,
+# and the output of the algorithm is a series of N-1 (x,y) integer 
+# offsets for each of the remaining images relative to this reference.
+def MTB(images):
+    pass
+
+
+if __name__ == "__main__":
+    pass
