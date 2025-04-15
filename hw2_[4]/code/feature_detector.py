8d9b42411e8c4a013f8a77714b9d45a4a26c57ec
Austin <austin030606@gmail.com>
2025-04-15 23:27:13 +0800

    finish keypoint candidate calculations (DoG local extrema)

--- hw2_[4]/code/feature_detector.py
+++ hw2_[4]/code/feature_detector.py
@@ -24,9 +24,13 @@ class SIFT(FeatureDetector):
         self.sigma = 1.6 # of gaussian kernels
         
     def detect(self, im):
-        # compute octaves of blurred images
+        # compute octaves
         gaussian_octaves = self.compute_gaussian_octaves(im)
         DoG_octaves = self.compute_DoG_octaves(gaussian_octaves)
+        
+        # locate local extrema
+        keypoint_candidates = self.compute_keypoint_candidates(DoG_octaves)
+                            
         return []
     
     def compute_gaussian_octaves(self, im):
@@ -56,6 +60,46 @@ class SIFT(FeatureDetector):
                 cur_octave.append(cur_gaussian_octave[i + 1] - cur_gaussian_octave[i])
             
             octaves.append(cur_octave)
-            print(len(cur_octave))
         
         return octaves
+    
+    def compute_keypoint_candidates(self, DoG_octaves):
+        offsets = []
+        for dk in range(-1, 2):
+            for di in range(-1, 2):
+                for dj in range(-1, 2):
+                    if dk == 0 and di == 0 and dj == 0:
+                        continue
+                    offsets.append((dk, di, dj))
+
+        keypoint_candidates = []
+        for octave_idx, cur_octave in enumerate(DoG_octaves):
+            for k in range(1, self.s + 1):
+                for i in range(1, cur_octave[k].shape[0] - 1):
+                    for j in range(1, cur_octave[k].shape[1] - 1):
+                        sign = 0
+                        found_extrema = True
+                        for dk, di, dj in offsets:
+                            difference = cur_octave[k][i][j].astype(np.int32) - cur_octave[k + dk][i + di][j + dj]
+                            if difference == 0:
+                                found_extrema = False
+                                break
+                            else:
+                                if sign == 0:
+                                    sign = np.sign(difference)
+                                else:
+                                    cur_sign = np.sign(difference)
+                                    if cur_sign != sign:
+                                        found_extrema = False
+                                        break
+
+                        if found_extrema:
+                            keypoint_candidates.append((octave_idx, i, j, k))
+                        # neighbors = []
+                        # for dk, di, dj in offsets:
+                        #     neighbors.append(cur_octave[k + dk][i + di][j + dj])
+                        
+                        # if np.all(cur_octave[k][i][j] > neighbors) or np.all(cur_octave[k][i][j] < neighbors):
+                        #     keypoint_candidates.append((octave_idx, i, j, k))
+        # print(len(keypoint_candidates))
+        return keypoint_candidates
