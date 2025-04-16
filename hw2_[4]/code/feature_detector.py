39b761960aca8adbac4a51ae8bc4c3063ac097fe
Austin <austin030606@gmail.com>
2025-04-16 20:18:39 +0800

    finish accurate keypoint localization

--- hw2_[4]/code/feature_detector.py
+++ hw2_[4]/code/feature_detector.py
@@ -21,22 +21,33 @@ class SIFT(FeatureDetector):
             self):
         super().__init__()
         self.s = 3 # scale per octave
-        self.sigma = 1.6 # of gaussian kernels
+        self.sigma = 1.6 # for gaussian kernels
+        self.max_iterations = 8
+        self.contrast_threshold = 0.03
+        self.r = 10
+        self.extremum_values = []
         
     def detect(self, im):
         # compute octaves
-        gaussian_octaves = self.compute_gaussian_octaves(im)
+        im_f = im.copy().astype(np.float32) / 255.0
+        im_f = cv2.resize(im_f, (0, 0), fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+        gaussian_octaves = self.compute_gaussian_octaves(im_f)
         DoG_octaves = self.compute_DoG_octaves(gaussian_octaves)
         
         # locate local extrema
         keypoint_candidates = self.compute_keypoint_candidates(DoG_octaves)
 
         adjusted_keypoint_candidates, keypoint_offsets = self.compute_keypoint_offsets(keypoint_candidates, DoG_octaves)
+        # values = np.array(self.extremum_values)
+        # print(values.min(), values.max(), values.mean())
+        # plt.hist(values)
+        # plt.show()
         return []
     
     def compute_gaussian_octaves(self, im):
         octaves = []
-        cur_base_im = cv2.GaussianBlur(im, (0, 0), self.sigma)
+        # cur_base_im = cv2.GaussianBlur(im, (0, 0), self.sigma)
+        cur_base_im = im
         k = 2 ** (1 / self.s)
 
         while min(cur_base_im.shape[0], cur_base_im.shape[1]) >= 16:
@@ -44,11 +55,11 @@ class SIFT(FeatureDetector):
 
             for i in range(1, self.s + 3):
                 cur_octave.append(cv2.GaussianBlur(cur_base_im, (0, 0), (k ** i) * self.sigma))
-            
+                
             octaves.append(cur_octave)
             cur_base_im = cur_octave[-3]
             cur_base_im = cv2.resize(cur_base_im, (0, 0), fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
-            
+          
         return octaves
     
     def compute_DoG_octaves(self, gaussian_octaves):
@@ -81,7 +92,7 @@ class SIFT(FeatureDetector):
                         sign = 0
                         found_extrema = True
                         for dk, di, dj in offsets:
-                            difference = cur_octave[k][i][j].astype(np.int32) - cur_octave[k + dk][i + di][j + dj]
+                            difference = cur_octave[k][i][j] - cur_octave[k + dk][i + di][j + dj]
                             if difference == 0:
                                 found_extrema = False
                                 break
@@ -110,34 +121,90 @@ class SIFT(FeatureDetector):
         offsets = []
 
         for octave_idx, i, j, k in keypoint_candidates:
-            D = DoG_octaves[octave_idx]
-
+            found_valid_keypoint_candidate = False
             x = j
             y = i
             s = k
+            offset = np.zeros(3)
+            D = DoG_octaves[octave_idx]
+            for iteration in range(self.max_iterations):
+                D_x = (D[s][y][x + 1] - D[s][y][x - 1]) / 2.0
+                D_y = (D[s][y + 1][x] - D[s][y - 1][x]) / 2.0
+                D_s = (D[s + 1][y][x] - D[s - 1][y][x]) / 2.0
 
-            D_x = (1.0 * D[s][y][x + 1] - D[s][y][x - 1]) / 2.0
-            D_y = (1.0 * D[s][y + 1][x] - D[s][y - 1][x]) / 2.0
-            D_s = (1.0 * D[s + 1][y][x + 1] - D[s - 1][y][x]) / 2.0
+                D_xx = D[s][y][x + 1] - 2.0 * D[s][y][x] + D[s][y][x - 1]
+                D_yy = D[s][y + 1][x] - 2.0 * D[s][y][x] + D[s][y - 1][x]
+                D_ss = D[s + 1][y][x] - 2.0 * D[s][y][x] + D[s - 1][y][x]
 
-            D_xx = D[s][y][x + 1] - 2.0 * D[s][y][x] + D[s][y][x - 1]
-            D_yy = D[s][y + 1][x] - 2.0 * D[s][y][x] + D[s][y - 1][x]
-            D_ss = D[s + 1][y][x] - 2.0 * D[s][y][x] + D[s - 1][y][x]
+                D_xy = (D[s][y - 1][x - 1] - D[s][y + 1][x - 1] - D[s][y - 1][x + 1] + D[s][y + 1][x + 1]) / 4.0
+                D_xs = (D[s - 1][y][x - 1] - D[s + 1][y][x - 1] - D[s - 1][y][x + 1] + D[s + 1][y][x + 1]) / 4.0
+                D_ys = (D[s - 1][y - 1][x] - D[s + 1][y - 1][x] - D[s - 1][y + 1][x] + D[s + 1][y + 1][x]) / 4.0
 
-            D_xy = (1.0 * D[s][y - 1][x - 1] - D[s][y + 1][x - 1] - D[s][y - 1][x + 1] + D[s][y + 1][x + 1]) / 4.0
-            D_xs = (1.0 * D[s - 1][y][x - 1] - D[s + 1][y][x - 1] - D[s - 1][y][x + 1] + D[s + 1][y][x + 1]) / 4.0
-            D_ys = (1.0 * D[s - 1][y - 1][x] - D[s + 1][y - 1][x] - D[s - 1][y + 1][x] + D[s + 1][y + 1][x]) / 4.0
+                gradient = np.array([D_x, D_y, D_s])
+                
+                Hessian = np.array([
+                    [D_xx, D_xy, D_xs],
+                    [D_xy, D_yy, D_ys],
+                    [D_xs, D_ys, D_ss]
+                ])
 
-            gradient = np.array([D_x, D_y, D_s])
+                # det = np.linalg.det(Hessian)
+                # if abs(det) < 1e-12:
+                #     break
+                Hessian_inverse = np.linalg.inv(Hessian)
+
+                offset = -1.0 * Hessian_inverse.dot(gradient)
+
+                if np.sum(np.abs(offset) > 0.5) == 0:
+                    found_valid_keypoint_candidate = True
+                    break
+                else:
+                    if offset[0] > 0.5:
+                        x += 1
+                    elif offset[0] < -0.5:
+                        x -= 1
+                    
+                    if offset[1] > 0.5:
+                        y += 1
+                    elif offset[1] < -0.5:
+                        y -= 1
+                    
+                    if offset[2] > 0.5:
+                        s += 1
+                    elif offset[2] < -0.5:
+                        s -= 1
+
+                    # x += int(np.round(offset[0]))
+                    # y += int(np.round(offset[1]))
+                    # s += int(np.round(offset[2]))
+
+                    if x < 1 or x > D[0].shape[1] - 2 or y < 1 or y > D[0].shape[0] - 2 or s < 1 or s > self.s:
+                        break
             
-            Hessian = np.array([
-                [D_xx, D_xy, D_xs],
-                [D_xy, D_yy, D_ys],
-                [D_xs, D_ys, D_ss]
-            ])
-
-            Hessian_inverse = np.linalg.inv(Hessian)
-
-            offset = -1.0 * Hessian_inverse.dot(gradient)
-            # print(offset)
+            if found_valid_keypoint_candidate:
+                if not self.is_unstable_or_an_edge(x, y, s, offset, D):
+                    adjusted_keypoint_candidates.append((octave_idx, y, x, s))
+                    offsets.append(offset)
+            else:
+                # print(f"failed to find valid keypoint candidate after {self.max_iterations} iterations", offset)
+                # print(f"{np.sum(np.abs(offset) > 0.5)} dimension > 0.5", offset)
+                pass
+        print(len(adjusted_keypoint_candidates), len(offsets))
+        print(len(keypoint_candidates))
         return adjusted_keypoint_candidates, offsets
+    
+    def is_unstable_or_an_edge(self, x, y, s, offset, D):
+        D_x = (D[s][y][x + 1] - D[s][y][x - 1]) / 2.0
+        D_y = (D[s][y + 1][x] - D[s][y - 1][x]) / 2.0
+        D_s = (D[s + 1][y][x] - D[s - 1][y][x]) / 2.0
+        gradient = np.array([D_x, D_y, D_s])
+        extremum = D[s][y][x] + 0.5 * gradient.dot(offset)
+
+        D_xx = D[s][y][x + 1] - 2.0 * D[s][y][x] + D[s][y][x - 1]
+        D_yy = D[s][y + 1][x] - 2.0 * D[s][y][x] + D[s][y - 1][x]
+        D_xy = (D[s][y - 1][x - 1] - D[s][y + 1][x - 1] - D[s][y - 1][x + 1] + D[s][y + 1][x + 1]) / 4.0
+
+        Tr_Hessian = D_xx + D_yy
+        Det_Hessian = D_xx * D_yy - D_xy * D_xy
+        self.extremum_values.append(np.abs(extremum))
+        return np.abs(extremum) * self.s < self.contrast_threshold or ((Tr_Hessian * Tr_Hessian) / Det_Hessian) >= ((self.r + 1) ** 2) / self.r
