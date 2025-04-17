c19a7f4cf2b04334a20f0a1ebebc1daf42f66cad
vannpacks <storm100006@gmail.com>
2025-04-17 21:52:26 +0800

    add feature matching

--- hw2_[4]/code/homography.py
+++ hw2_[4]/code/homography.py
@@ -9,26 +9,8 @@ import matplotlib.pyplot as plt
 from scipy.spatial import cKDTree
 from collections import defaultdict
 import glob
+from feature_matching import *
 from scipy.optimize import least_squares
-
-
-def extract_features(img):
-    # img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    sift = cv2.SIFT_create()
-    keypoints, descriptors = sift.detectAndCompute(img, None)
-    return keypoints, descriptors
-
-def match_features(desc1, desc2, ratio_thresh=0.75):
-    tree = cKDTree(desc2)
-    distances, indices = tree.query(desc1, k=2)
-
-    matches = []
-    for i, (d1, d2) in enumerate(distances):
-        if d1 < ratio_thresh * d2:
-            match = cv2.DMatch(_queryIdx=i, _trainIdx=indices[i][0], _distance=d1)
-            matches.append(match)
-    return matches
-
 
 
 def similarity_transform(points):
